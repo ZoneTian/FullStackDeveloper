6708fa035540725410b2b66b05c9d100feb645af
zoneTian <Tian.Zhao@connext.com.cn>
2020-04-22 11:02:24 +0800

    react-redux

--- src/pages/ReduxPage.js
+++ src/pages/ReduxPage.js
@@ -1,36 +1,42 @@
 import React, { Component } from 'react'
-import store from '../store'
-export default class ReduxPage extends Component {
-  componentDidMount(){
-    //store里的store变化 执行callback
+import { connect } from 'react-redux'
 
-   this.unsubscribe =   store.subscribe(()=>{
-      this.forceUpdate()
-    })
-  }
-  componentWillUnmount(){
-    this.unsubscribe &&   this.unsubscribe()
-  }
+// import store from '../store'
+
+@connect(
+  ({home})=>({count:home}),
+)
+class ReduxPage extends Component {
+  // componentDidMount(){
+  //   //store里的store变化 执行callback
+
+  //  this.unsubscribe =   store.subscribe(()=>{
+  //     this.forceUpdate()
+  //   })
+  // }
+  // componentWillUnmount(){
+  //   this.unsubscribe &&   this.unsubscribe()
+  // }
   add=()=>{
-      store.dispatch({type:'ADD',payload:1})
+      this.props.dispatch({type:'ADD',payload:1})
   }
-  Aysnadd = ()=>{
-    store.dispatch((dispatch,getState)=>{
-      setTimeout(()=>{
-        store.dispatch({type:'ADD',payload:100})
-      },1000)
-    })
-  }
-  promiseMinus=()=>{
+  // Aysnadd = ()=>{
+  //   store.dispatch((dispatch,getState)=>{
+  //     setTimeout(()=>{
+  //       store.dispatch({type:'ADD',payload:100})
+  //     },1000)
+  //   })
+  // }
+  // promiseMinus=()=>{
     
-    store.dispatch(Promise.resolve({type:'MINUS',payload:100}))
-  }
+  //   store.dispatch(Promise.resolve({type:'MINUS',payload:100}))
+  // }
   render() {
-    
+    const { count} = this.props
     return (
       <div>
         <h3>reduxpage</h3>
-        <div >{store.getState().home}</div>
+        <div >{count}</div>
        <div><button onClick = {this.Aysnadd }> Aysnadd </button></div> 
        <div><button onClick = {this.promiseMinus }> promiseMinus</button></div> 
 
@@ -39,3 +45,4 @@ export default class ReduxPage extends Component {
     )
   }
 }
+export default  ReduxPage
